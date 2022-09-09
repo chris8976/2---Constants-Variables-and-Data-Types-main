61e32568c907ced0d9b91e5a4883f8530c41c8ee
Christopher Dominguez <Neymar15216@gmail.com>
2022-09-09 14:16:27 -0400

    1 lesson

--- Lab - Constants and Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
@@ -10,7 +10,11 @@
  
  For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
  */
-
+var likes = 454
+var comments = 99
+let year = 2004
+let month = 10
+let day = 15
 
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Fitness Tracker: Constant or Variable?](@next)
